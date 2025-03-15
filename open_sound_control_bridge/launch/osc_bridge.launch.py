afed37f9914191660e0f5ede92f32620b18a971d
Chris I-B <ve4cib@gmail.com>
2025-03-15 12:43:30 -0400

    Start filling in the body of the bridge node & launch file. Add a basic example configuration as the default. Add the resource directory

--- open_sound_control_bridge/launch/osc_bridge.launch.py
+++ open_sound_control_bridge/launch/osc_bridge.launch.py
@@ -12,3 +12,47 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+ARGUMENTS = [
+    DeclareLaunchArgument(
+        'osc_config',
+        default_value=f'{get_package_share_directory("open_sound_control_bridge")}/config/example_config.yaml',
+        description='Path to the OSC bridge configuration file',
+    ),
+    DeclareLaunchArgument(
+        'port',
+        default_value='9001',
+        description='UDP port we accept incoming OSC packets on',
+    ),
+]
+
+
+def generate_launch_description():
+    config_file = LaunchConfiguration('osc_config')
+    port = LaunchConfiguration('port')
+
+    osc_bridge_node = Node(
+        package='open_sound_control_bridge',
+        executable='osc_bridge',
+        name='osc_bridge',
+        output='screen',
+        arguments=[
+            '--config',
+            config_file,
+            '--port',
+            port
+        ]
+    )
+
+    # Create launch description and add actions
+    ld = LaunchDescription(ARGUMENTS)
+    ld.add_action(osc_bridge_node)
+    return ld
--- open_sound_control_bridge/open_sound_control_bridge/osc_bridge.py
+++ open_sound_control_bridge/open_sound_control_bridge/osc_bridge.py
@@ -13,8 +13,50 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from ament_index_python.packages import get_package_share_directory
+import argparse
+import rclpy
+from rclpy.node import Node
+
+class OscBridgeNode(Node):
+    def __init__(
+        self,
+        config_path: str,
+        udp_port: int,
+    ):
+        super().__init__('osc_bridge_node')
+        self.config_path = config_path
+        self.udp_port = udp_port
+
+
 def main():
-    pass
+    default_cfg = f'{get_package_share_directory("open_sound_control_bridge")}/config/example_config.yaml'
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-p',
+        '--port',
+        action='store',
+        dest='port',
+        type=int,
+        default=9001,
+        help='UDP port we accept OSC packets on (default: 9001)',
+    )
+    parser.add_argument(
+        '-c',
+        '--config',
+        action='store',
+        dest='config',
+        type=str,
+        default=default_cfg,
+        help=f'Path to the OSC bridge configuration file (default: {default_cfg})',
+    )
+    args = parser.parse_args()
+
+    rclpy.init()
+    node = OscBridgeNode(args.config, args.port)
+    rclpy.spin(node)
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
